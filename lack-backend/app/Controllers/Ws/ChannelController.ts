a521b1bc47c4e288c8b2c672f8216f884b0fe42f
Max <doomymax@gmail.com>
2022-05-08 12:16:00 +0800

    Remove logs and cleanup

--- lack-backend/app/Controllers/Ws/ChannelController.ts
+++ lack-backend/app/Controllers/Ws/ChannelController.ts
@@ -41,7 +41,6 @@ export default class ChannelController {
     const user = auth.user!
     const channel = await this.channelRepository.findByName(channelName)
     if (channel.adminId === user.id) {
-      console.log('channel admin left - deleting channel')
       await this.channelRepository.delete(channel)
       socket.nsp.emit('channelDeleted', channel.name)
     } else {
@@ -52,7 +51,6 @@ export default class ChannelController {
 
   public async deleteChannel({ auth, params, socket }: WsContextContract) {
     const channelName = params.name
-    console.log('deleteChannel', channelName)
     const user = auth.user!
     const channel = await this.channelRepository.findByName(channelName)
     const errParams = {
@@ -74,7 +72,6 @@ export default class ChannelController {
     channelParam: string,
     userParam: string
   ) {
-    console.log('inviteUser', channelParam, userParam)
     const channel = await this.channelRepository.findByName(channelParam)
     const userToGetInvited = await this.userRepository.findByNickname(userParam)
     const error = checkForErrors(
@@ -126,7 +123,6 @@ export default class ChannelController {
     userParam: string,
     isRevoke: boolean
   ) {
-    console.log('kickUser', userParam)
     const kicker = auth.user!
     const channel = await this.channelRepository.findByName(params.name)
     const userToKick = await this.userRepository.findByNickname(userParam)
